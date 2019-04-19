c122fc3360a4e914d9366d5a51a62bd3a1a0104e
Ali Moazenzadeh <alimz@dr.com>
2019-04-19 16:08:59 +0430

    Initial Commit

---
+++ TagBar/TagBar.h
@@ -0,0 +1,19 @@
+//
+//  TagBar.h
+//  TagBar
+//
+//  Created by Ali`s Macbook Pro on 4/19/19.
+//  Copyright © 2019 alimz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TagBar.
+FOUNDATION_EXPORT double TagBarVersionNumber;
+
+//! Project version string for TagBar.
+FOUNDATION_EXPORT const unsigned char TagBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TagBar/PublicHeader.h>
+
+
